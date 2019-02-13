1066e446ead8acf1f7441e1aeefb0de33bbd5cc4
sean <smgelbgithub@gmail.com>
2019-02-13 00:05:51 -0500

    email fix syntax

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = '<your heroku app>.herokuapp.com'
-  config.action_mailer.default_url_options = { host: www.gelysium.com }
+  config.action_mailer.default_url_options = { host: 'www.gelysium.com' }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
