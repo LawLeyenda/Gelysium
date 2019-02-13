99768c98fd590e9446bd2b9595eda799c6f04f0f
sean <smgelbgithub@gmail.com>
2019-02-12 23:53:21 -0500

    Chp 11 - Account activation working in development

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,8 +37,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-
-  # Print deprecation notices to the stderr.
+  config.action_mailer.default_url_options = { host: 'example.com' }
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -12,10 +12,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should redirect index when not logged in" do
-    get users_path
-    assert_redirected_to login_url
-  end
 
   test "should redirect edit when not logged in" do
     get edit_user_path(@user)
@@ -29,4 +25,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+        user: { password:              @other_user.password_digest,
+                password_confirmation: @other_user.password_digest,
+                admin: @other_user.admin? } }
+  end
+
 end
