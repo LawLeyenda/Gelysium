545d13dfec8f805084ad4342641c8e68fd6af428
sean <smgelbgithub@gmail.com>
2019-03-18 01:03:18 -0400

    add user microposts.3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
         # Configuration for Amazon S3
         :provider              => 'AWS',
-        :aws_access_key_id     => ENV['AKIAJ2DMMB6E2WJ2QYWQ'],
-        :aws_secret_access_key => ENV['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']
+        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+        :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['arn:aws:s3:::sample-app-bucket-images']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
