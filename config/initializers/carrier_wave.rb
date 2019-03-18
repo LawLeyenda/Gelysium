a8fa0973c83836bd0508eb75ce9d84e16e9ac9c2
sean <smgelbgithub@gmail.com>
2019-03-18 03:54:19 -0400

    add user microposts.14

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,13 @@
+
 if Rails.env.production?
   CarrierWave.configure do |config|
-    # config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-        #Configuration for Amazon S3
-        :provider		=> 'AWS',
-        :aws_access_key_id		=> ENV['AWS_ACCESS_KEY'],
-        :aws_secret_access_key 	=> ENV['AWS_SECRET_KEY'],
-        :region                => ENV['AWS_REGION']
+        # Configuration for Amazon S3
+        :provider              => 'AWS',
+        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+        :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+        :region                => ENV['S3_REGION']
     }
-    config.fog_directory= ENV['AWS_BUCKET']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
